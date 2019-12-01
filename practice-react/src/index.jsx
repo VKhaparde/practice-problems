ccb70a09449eead7e2c982e95c36b00c085c6f06
Vandana Khaparde <vandana.khaparde@gmail.com>
2019-12-01 22:50:03 +0000

    Was working with React-Router EXamples -Vandana

--- practice-react/src/index.jsx
+++ practice-react/src/index.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import Calculator from './calculator';
 import ShowList from './list';
 import ExampleForm from './form';
-const numbers = [1, 2, 3, 4, 5];
+import App from './app';
+import App2 from './app2';
+// const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
   // <ShowList numbers={numbers}/>,
-  <ExampleForm/>,
+  // <ExampleForm/>,
+  // <App />,
+  <App2 />,
   document.querySelector('#root')
 );
